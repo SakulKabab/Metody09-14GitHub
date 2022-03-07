70e803c2da8b9bd98d049c110ccf61cc0cfab84f
babakl.03 <babakl.03@spst.eu>
2022-03-07 13:12:45 +0100

    metody11 Hotovo

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,38 @@ namespace Meotdy09_14GitHub
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = double.Parse(textBox3.Text);
+            double x1, x2, d;
+            Metody.Diskriminant(a, b, c, out x1, out x2, out d);
+            if (d < 0) MessageBox.Show("Rovnice nema reseni v R");
+            else if (d > 0) MessageBox.Show("Koren x1 je: " + x1 + " a koren x2 je: " + x2);
+            else MessageBox.Show("Rovnice ma jedno dvojnasobne reseni " + x1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox4.Text;
+            int cifSoucet = 0, lichCifSoucet = 0, sudCifSoucet = 0;
+            if (Metody.ObsahujeCislici(s1, out cifSoucet, out lichCifSoucet, out sudCifSoucet))
+            {
+                MessageBox.Show("Obsahuje číslici");
+                MessageBox.Show("Ciferný součet je " + cifSoucet);
+                MessageBox.Show("Ciferný součet lichých čísel je " + lichCifSoucet);
+                MessageBox.Show("Ciferný součet sudých čísel je " + sudCifSoucet);
+            }
+            else MessageBox.Show("Neobsahuje cifry");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string s = textBox5.Text;
+            int pocetSlov = Metody.PocetSlov(ref s);
+            MessageBox.Show("pocet slov je: " + pocetSlov + "\nOdstraněné číslice: " + s);
+        }
     }
 }
--- Metody.cs
+++ Metody.cs
@@ -8,6 +8,90 @@ namespace Meotdy09_14GitHub
 {
     class Metody
     {
+        public static double Diskriminant(double a, double b, double c, out double x1, out double x2, out double d)
+        {
+            d = (b * b) - (4 * a * c);
+            x1 = 0;
+            x2 = 0;
+            if (d < 0)
+            {
+                d = 0;
+            }
+            else if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+            }
+            return d;
+        }
+        public static bool ObsahujeCislici(string idk, out int cifSoucet, out int lichCifSoucet, out int sudCifSoucet)
+        {
+            bool obsahujeCislici = false;
+            cifSoucet = 0;
+            lichCifSoucet = 0;
+            sudCifSoucet = 0;
+            for (int i = 0; i < idk.Length; ++i)
+            {
+                if (Char.IsNumber(idk[i]))
+                {
+                    obsahujeCislici = true;
+                    cifSoucet += int.Parse(idk[i].ToString());
 
+                    if (int.Parse(idk[i].ToString()) % 2 != 0)
+                    {
+                        lichCifSoucet += int.Parse(idk[i].ToString());
+                    }
+                    else if (int.Parse(idk[i].ToString()) % 2 == 0)
+                    {
+                        sudCifSoucet += int.Parse(idk[i].ToString());
+                    }
+                }
+            }
+
+            return obsahujeCislici;
+        }
+        //int PocetSlov(string a, out string sUpraveno)
+        //{
+        //    int pocSlov = 0;
+        //    string[] sOddeleno;
+        //    char[] separator = { ' ' };
+        //    sOddeleno = a.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        //    pocSlov = sOddeleno.Length;
+
+        //    int i = 0;
+        //    while(i < a.Length - 1)
+        //    {
+        //        if (a[i] >= '0' && a[i] <= '9')
+        //        {
+        //            a = a.Remove(i, 1);
+        //        }
+        //        else ++i;
+        //    }
+        //    sUpraveno = a;
+        //    return pocSlov;
+        //}
+        public static int PocetSlov(ref string a)
+        {
+            int pocSlov = 0;
+            string[] sOddeleno;
+            char[] separator = { ' ' };
+            sOddeleno = a.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            pocSlov = sOddeleno.Length;
+
+            int i = 0;
+            while (i < a.Length - 1)
+            {
+                if (a[i] >= '0' && a[i] <= '9')
+                {
+                    a = a.Remove(i, 1);
+                }
+                else ++i;
+            }
+            return pocSlov;
+        }
     }
 }
